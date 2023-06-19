4ed014ada75685fd41c9c1b67970fcf733f816e8
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-19 14:57:22 +0700

    add styling for mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import dividerDesktopURL from './assets/pattern-divider-desktop.svg'
+import dividerMobileURL from './assets/pattern-divider-mobile.svg'
+import diceIconUrl from './assets/icon-dice.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <main>
+    <div>
+      <main className='advice'>
+        <h1 className='advice-number'>Advice #117</h1>
+        <q className='advice-quote' id='advice-quote'>It is easy to sit up and take notice, what's difficult is getting up and taking action.</q>
+        <img className='divider' src={dividerMobileURL} alt="divider with a single horizontal line dvided in half by two vertical lines" />
       
-    </main>
+      </main>
+      <button className='btn'><img className='dice-icon' src={diceIconUrl} alt="dice icon" /></button>
+    </div>
   )
 }
 
