9d2842ffd67659551d09fff0056ecb6bba40cb78
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-06-20 14:49:34 +0700

    add functions to get data from API and display on page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,38 @@ import dividerMobileURL from './assets/pattern-divider-mobile.svg'
 import diceIconUrl from './assets/icon-dice.svg'
 
 function App() {
+  const dividerUrl = window.innerWidth < '600px' ? dividerMobileURL : dividerDesktopURL
+  const [adviceData, setAdviceData] = useState({
+    advice: "If you don't want something to be public, don't post it on the Internet.",
+    number: 85
+  })
+  const [slipId, setSlipId] = useState(85)
 
+  useEffect(() => {
+    fetch(`https://api.adviceslip.com/advice/${slipId}`)
+      .then(res => res.json())
+      .then(data => {
+        setAdviceData(prevAdviceData => ({
+          advice: data.slip.advice,
+          number: data.slip.id
+        }))
+      })
+  }, [slipId])
+
+  function handleClick() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    setSlipId(randomNumber)
+  }
+   
   return (
-    <div>
-      <main className='advice'>
-        <h1 className='advice-number'>Advice #117</h1>
-        <q className='advice-quote' id='advice-quote'>It is easy to sit up and take notice, what's difficult is getting up and taking action.</q>
-        <img className='divider' src={dividerMobileURL} alt="divider with a single horizontal line dvided in half by two vertical lines" />
+    <div className='container'>
+      <div className='advice'>
+        <h1 className='advice-number'>Advice #{adviceData.number}</h1>
+        <q className='advice-quote' id='advice-quote'>{adviceData.advice}</q>
+        <img className='divider' src={dividerUrl} alt="divider with a single horizontal line dvided in half by two vertical lines" />
       
-      </main>
-      <button className='btn'><img className='dice-icon' src={diceIconUrl} alt="dice icon" /></button>
+      </div>
+      <button className='btn' onClick={handleClick}><img className='dice-icon' src={diceIconUrl} alt="dice icon" /></button>
     </div>
   )
 }
